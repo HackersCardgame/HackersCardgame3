89867242c2cd5e0887e35f4280dcd0cc48414017
Your Name <you@example.com>
2019-10-23 21:28:16 +0200

    tempsave

--- jpg/EN/index.php
+++ jpg/EN/index.php
@@ -43,3 +43,4 @@ if (in_array($ext, $supported_file)) {
 ?>
 
 <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/619062402&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/611081637&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
