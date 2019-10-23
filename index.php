7faa80bc9b304021a2b989b02f7f6861a77530ba
Your Name <you@example.com>
2019-10-23 22:08:36 +0200

    tempsave

--- index.php
+++ index.php
@@ -40,8 +40,8 @@ if (in_array($ext, $supported_file)) {
 
     echo '<div style="float:left;text-align:center;">';
     $parts = explode(".", $image);
-    echo "<font color=green>" . $parts[0] . "</font>" . "<br>";
-    echo '<a href="./'.$image .'" id="'.$parts[0].'"'.' target="_blank"><img width=200 src="'.$image .'" ></a><br><hr>';
+    //echo "<font color=green>" . $parts[0] . "</font>" . "<br>";
+    echo '<a href="./'.$image .'" id="'.$parts[0].'"'.' target="_blank"><img width=200 src="'.$image .'" ></a><br><br>';
     echo '</div>
 ';
 
