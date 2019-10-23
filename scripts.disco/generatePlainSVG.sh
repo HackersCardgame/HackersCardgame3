366a6f2466e77fe7d189f780404153457d4b1f5b
Your Name <you@example.com>
2019-10-23 15:01:33 +0200

    tempsave

---
+++ scripts.disco/generatePlainSVG.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+clear
+
+if  [ "$1" = "" ]
+then
+echo -e "
+\e[39m
+Usage:
+------
+To generate the Enlgish PNGs sheets: \e[36m./generatePNGs.sh EN\e[39m
+To generate the German  PNGs sheets: \e[36m./generatePNGs.sh DE\e[39m
+
+
+"
+exit 0
+fi
+
+
+clear
+
+
+find ../cards/$1 -name "*.svg" >./SVGs.txt
+
+svgpath=$(dirname $(pwd))/svg
+
+mkdir -p $svgpath/$1
+
+counter=0
+
+for i in $(cat ./SVGs.txt)
+  do
+  let counter=counter+1
+  /usr/bin/inkscape $i &
+  sleep 5
+  xdotool key Ctrl+Alt+a
+  sleep 5
+  xdotool key Ctrl+Shift+s
+  sleep 5
+  xdotool key Shift+Tab Shift+Tab Shift+Tab Down
+  sleep 5
+  xdotool key Tab Tab Tab
+  sleep 5
+  echo $svgpath/$1/
+  xdotool type "$svgpath/$1/"
+  sleep 5
+  xdotool type $(basename $i .svg)
+  sleep 5
+  xdotool key KP_Enter
+  sleep 5
+  xdotool key Ctrl+w
+  sleep 5
+  xdotool key Alt+F4
+  sleep 5
+  xdotool key Alt+w
+done
+
+
+
