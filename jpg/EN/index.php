b7fda941b22bd85b02248877342376148b4f5e12
Your Name <you@example.com>
2019-10-23 21:42:56 +0200

    tempsave

--- jpg/EN/index.php
+++ jpg/EN/index.php
@@ -11,6 +11,12 @@ Source:<br>
 
 <!-- and of course nobody would have the stupid idea to place microdots in such a repository https://en.wikipedia.org/wiki/Microdot-->
 
+
+<audio onloadeddata="this.play();" poster="a1.jpg" playsinline loop controls>
+    <source src="SpaceTripExtended.mp3" type="audio/mp3" />
+    Your browser does not support the video tag or the file format of this video.
+</audio>
+
 <br><br>
    <?php
 
@@ -42,7 +48,7 @@ if (in_array($ext, $supported_file)) {
 
 ?>
 
-<audio onloadeddata="this.play();" poster="a1.jpg" playsinline loop controls>
+<audio onloadeddata="this.play();" poster="a1.jpg" playsinline loop muted controls>
     <source src="SpaceTripExtended.mp3" type="audio/mp3" />
     Your browser does not support the video tag or the file format of this video.
 </video>
