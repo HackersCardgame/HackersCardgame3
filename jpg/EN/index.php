b16c5165dc05db1a7581cf3f6484b6d819ff1715
Your Name <you@example.com>
2019-10-26 08:51:29 +0200

    tempsave

--- jpg/EN/index.php
+++ jpg/EN/index.php
@@ -9,7 +9,7 @@ Source:<br>
 <a href="https://github.com/braindef/Hackers-Cardgame2">https://github.com/braindef/Hackers-Cardgame2</a><br>
 <a href="https://github.com/braindef/Hackers-Cardgame3">https://github.com/braindef/Hackers-Cardgame3</a><br>
 <a href="https://2hmorvqnlhwyrhvl6dwv4jqgnvhlaf2wxnrbmbhrbpmx5x3qqiwprjyd.onion/">https://2hmorvqnlhwyrhvl6dwv4jqgnvhlaf2wxnrbmbhrbpmx5x3qqiwprjyd.onion/</a>
-<a href="https://github.com/braindef/HackersCardgame3/tree/master/pdf/EN/A4">PRINTING TEMPLATES (PDF)</a>
+<br><br><a href="https://github.com/braindef/HackersCardgame3/tree/master/pdf/EN/A4">PRINTING TEMPLATES (PDF)</a>
 
 <!-- and of course nobody would have the stupid idea to place microdots in such a repository https://en.wikipedia.org/wiki/Microdot-->
 
