cd5cbe4cad8c3f7469d390185f8c628b2ca3c598
Your Name <you@example.com>
2019-10-26 08:48:04 +0200

    tempsave

--- jpg/DE/index.php
+++ jpg/DE/index.php
@@ -1,17 +1,25 @@
 <h1>Hackers Cardgame</h1>
 Never Play "Red Team" help out with "Blue Team" and save the World<br><br>
-please respect the license, People like Julian, Fefe, Frank, Paniq, Silvia... (just ask) are excluded from the 1978 Rule<br><br>
+please respect the license, People like Julian, Fefe, Frank, Paniq, Silvia, Gunnar... (just ask) are excluded from the 1978 Rule<br><br>
 <a href="https://github.com/braindef/Hackers-Cardgame/blob/master/LICENSE">license</a>
 
 
 Source:<br>
 <a href="https://github.com/braindef/Hackers-Cardgame">https://github.com/braindef/Hackers-Cardgame</a><br>
 <a href="https://github.com/braindef/Hackers-Cardgame2">https://github.com/braindef/Hackers-Cardgame2</a><br>
+<a href="https://github.com/braindef/Hackers-Cardgame3">https://github.com/braindef/Hackers-Cardgame3</a><br>
 <a href="https://2hmorvqnlhwyrhvl6dwv4jqgnvhlaf2wxnrbmbhrbpmx5x3qqiwprjyd.onion/">https://2hmorvqnlhwyrhvl6dwv4jqgnvhlaf2wxnrbmbhrbpmx5x3qqiwprjyd.onion/</a>
 
 <!-- and of course nobody would have the stupid idea to place microdots in such a repository https://en.wikipedia.org/wiki/Microdot-->
 
+<br>
+<audio onloadeddata="this.play();" poster="a1.jpg" playsinline loop muted controls>
+    <source src="SpaceTripExtended.mp3" type="audio/mp3" />
+    Your browser does not support the video tag or the file format of this video.
+</audio>
+
 <br><br>
+
    <?php
 
    $files = glob("*.*");
@@ -30,8 +38,12 @@ $supported_file = array(
 
 $ext = strtolower(pathinfo($image, PATHINFO_EXTENSION));
 if (in_array($ext, $supported_file)) {
-    echo '<a href="./'.$image .'"><img width=200 src="'.$image .'" ></a>';
-    echo '
+
+    echo '<div style="float:left;text-align:center;">';
+    $parts = explode(".", $image);
+    //echo "<font color=green>" . $parts[0] . "</font>" . "<br>";
+    echo '<a href="./'.$image .'" id="'.$parts[0].'"'.' target="_blank"><img width=200 src="'.$image .'" ></a><br>';
+    echo '</div>
 ';
 
 } else {
@@ -42,4 +54,3 @@ if (in_array($ext, $supported_file)) {
 
 ?>
 
-<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/619062402&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
