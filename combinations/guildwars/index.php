1bbfff5df7b6a1c5bf935656dc1777d0c814091a
Your Name <you@example.com>
2019-10-31 02:56:44 +0100

    tempsave

---
+++ combinations/guildwars/index.php
@@ -0,0 +1,42 @@
+
+<h1>GuildWars, or general MMORPG</h1>
+Working through <a href="../../jpg/EN/">all the cards</a> may help to find out what went wrong.<br><br>
+I should not have studied Computer Science and Psychology... After i found out, about the technology cards (t1, t2, ...) it was clear to me, that MMORPG's are beeing used to train bad stimulus-response-habits (t11 card or more likely t2 or t7 card). And then make the Kid that plays the game kill an equal aged. (= dt1 card). Of course a single kill in the game will not kill a human in the real world, otherweise gamers would have wiped out our planet a million times since there are billions of gamers with 10'000 game kills, but normally they switch between "this is only game" and "this will have impact on the real world". In a group they normally declare one kid to a "Rapist Terrorist" (m41 card) and make the others attack him / her (m37 card). I tried to explain the problem to a friend (you) but i guess they attacked him with a card that goes against his sexual integrity (and maybe co6 card) to create avoidance behavior to listen to me. <s>they</s>I also asked his Sister to act, but she did not want to. The last move they played against me with puting me again in a mental institution (m19 card), erasing my brain, making me forget about him, continuing <s>train</s> manipulate him.
+
+but maybe i'm wrong that they would like to do evil things if you look at the last sentence of this article: https://wowwiki.fandom.com/de/wiki/Nachtwache but gamers should know about the technology cards!!!
+
+<br>
+<br>
+<br>
+
+
+   <?php
+
+   $files = glob("*.*");
+
+  for ($i=count($files)-200; $i<count($files); $i++)
+
+{
+
+$image = $files[$i];
+$supported_file = array(
+    'gif',
+    'jpg',
+    'jpeg',
+    'png'
+);
+
+$ext = strtolower(pathinfo($image, PATHINFO_EXTENSION));
+if (in_array($ext, $supported_file)) {
+    echo '<a href="./'.$image .'"><img width=200 src="'.$image .'" ></a>';
+    echo '
+';
+
+} else {
+    continue;
+ }
+
+}
+
+?>
+
